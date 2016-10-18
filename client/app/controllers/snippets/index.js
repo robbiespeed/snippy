cfdbe89319f6a930d2c2d9f02d275b47a09c47bc
Robbie Speed <robert.y.speed@gmail.com>
2016-10-18 02:56:15 -0300

    make search case not sensitive; also hello `.torus.json`

--- client/app/controllers/snippets/index.js
+++ client/app/controllers/snippets/index.js
@@ -8,7 +8,7 @@ export default Ember.Controller.extend({
   searchTagsString: '',
 
   searchTags: computed('searchTagsString', function () {
-    return this.get('searchTagsString').split(', ');
+    return this.get('searchTagsString').toLowerCase().split(', ');
   }),
 
   currentSnippets: computed('allSnippets.content', function () {
@@ -25,16 +25,22 @@ export default Ember.Controller.extend({
   filteredSnippets: computed(
     'currentSnippets.@each.{name,tags}', 'searchNameString', 'searchTags',
     function () {
-      const searchName = this.get('searchNameString');
+      const searchName = this.get('searchNameString').toLowerCase();
       const searchTags = this.get('searchTags');
 
       return this.get('currentSnippets').filter((snippet) => {
-        const name = snippet.get('name');
-        const tags = snippet.get('tags');
+        const name = snippet.get('name').toLowerCase();
+        const tagNames = snippet.get('tags').reduce((reduced, tag) => {
+          const tagName = tag.get('name');
+          if (tagName) {
+            reduced.push(tagName.toLowerCase());
+          }
+          return reduced;
+        }, []);
 
         const nameMatch = !searchName.length || name.includes(searchName);
         const tagMatch = searchTags[0] === '' ||
-          searchTags.every((searchTag) => tags.isAny('name', searchTag));
+          searchTags.every((searchTag) => tagNames.includes(searchTag));
 
         return nameMatch && tagMatch;
       });
